b7d526f0c5aa514b6c5cefed76196ea86b5fb3eb
Kevin Xiwei Zheng <blankplacement@gmail.com>
2015-08-13 21:22:04 -0400

    PyAV on Travis CI, take two

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -2,7 +2,6 @@
 
 pkg-config --exists libavformat
 if [ $? != 0 ]; then
-    apt-get build-dep ffmpeg
     FFMPEG_SRC=$(mktemp -d)
     cd $FFMPEG_SRC
     curl -s http://ffmpeg.org/releases/ffmpeg-2.7.2.tar.bz2 | tar xj
