f22317633805244b2c21e452146338cca3e82151
Kevin Xiwei Zheng <blankplacement@gmail.com>
2015-08-16 21:49:00 -0400

    Try using Travis CI standard infrastructure

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -2,13 +2,14 @@
 
 pkg-config --exists libavformat
 if [ $? != 0 ]; then
+    sudo apt-get build-dep -qq --fix-missing ffmpeg
     FFMPEG_SRC=$(mktemp -d)
     cd $FFMPEG_SRC
     curl -s http://ffmpeg.org/releases/ffmpeg-2.7.2.tar.bz2 | tar xj
     cd ffmpeg-2.7.2
     ./configure
     make
-    make install
+    sudo make install
 else
     echo 'libavformat found; skipping ffmpeg build'
 fi
